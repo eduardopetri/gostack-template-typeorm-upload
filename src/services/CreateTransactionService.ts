862b27666130a705e8cd79d3e84ff73cc23ae192
eduardopetri <eduardoafonsopetri@gmail.com>
2020-11-10 21:00:59 -0300

    done challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new AppError('Invalid balance');
+    }
+
+    const findCategoryWithSameName = await categoryRepository.findOne({
+      title: category,
+    });
+
+    let categoryId = '';
+
+    if (!findCategoryWithSameName) {
+      const createCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(createCategory);
+      categoryId = createCategory.id;
+    } else {
+      categoryId = findCategoryWithSameName.id;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
